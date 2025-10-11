08b0198814884b0aad6bf4a90c98eae2f9a57614
yy <111@qq.comqq.com>
2025-10-11 22:47:50 +0800

    1.4

--- tvbox/yhdm.js
+++ tvbox/yhdm.js
@@ -1,31 +1,33 @@
 var rule={
-    title:'樱花动漫',
-    host:'https://www.857yhw.com/',
+    title: '穷次元',
+    host: 'https://jciyuan.com',
     // homeUrl:'/',
-    url:'/type/fyclass-fypage.html',
-    searchUrl: '/search/**----------fypage---.html',
-    filterable:0,//是否启用分类筛选,
-    searchable:2,//是否启用全局搜索,
-    quickSearch:0,//是否启用快速搜索,   
-    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
-        'User-Agent':'MOBILE_UA',
+    url: '/acgtype/fypage.html',
+    searchUrl: '/acgsearch/**-------------.html',
+    searchable: 2,
+    quickSearch: 0,
+    filterable: 1,
+    headers: {//网站的请求头,完整支持所有的,常带ua和cookies
+        'User-Agent': 'MOBILE_UA',
         // "Cookie": "searchneed=ok"
-    }, 
-    class_parse:'myui-header__menu nav-menu&&li;a&&Text;/(\\d+).html',
-    class_exaclude:'动漫资讯',
-    play_parse:true,
-    lazy:"",
-    limit:6,    
-    推荐:'myui-vodlist__box;a&&title;img&&data-original;a&&href',
-    一级:'.module-list&&.list-item;img&&alt;img&&data-original;a&&href',
-    二级访问前:'',
-    二级:{
-        "title":".info-title&&Text",
-        "img":".info-pic&&img&&data-original",
-        "desc":".info-intro&&Text",
-        "content":".info-short&&Text;.info-tag:eq(0)&&Text;.info-tag:eq(1)&&Text;.info-tag:eq(2)&&Text",
-        "tabs":".tab-item",
-        "lists":".playlist&&ul"
     },
-    搜索:'*',
+    class_parse: '.navbar&&div&&ul&&li;a&&title;a&&href;/(\\d+).html',
+    play_parse: true,
+    lazy: '',
+    limit: 6,
+    推荐: '.module-item-cover;a&&Text;a&&href;img&&data-original',
+    一级: '.module-item-cover;a&&Text;a&&href;img&&data-original',
+    二级: {
+        "title": "h1&&Text",
+        "img": ".module-info-pic&&img&&data-original",
+        "desc": ".module-info-introduction&&Text",
+        "content": ".module-info-item:eq(0)&&Text;.module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text",
+        "tabs": ".module-tab-item",
+        "lists": ".module-list:eq(#id)&&li"
+    },
+    搜索: '.module-item-cover;a&&Text;a&&href;img&&data-original',
+    
+
+    
+   
 }
