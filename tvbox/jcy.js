b561efd9c220f91bf34149609371c2096401ec18
yy <111@qq.comqq.com>
2025-10-12 23:29:29 +0800

    3.3

--- tvbox/jcy.js
+++ tvbox/jcy.js
@@ -26,7 +26,7 @@ var rule = {
         "img": ".module-item-pic&&src",
         "content": "",//简介
         "tabs": "#y-playList&&div",//线路名
-        "lists": "#panel1:eq(#id)&&div&&div&&a"//列表
+        "lists": "#panel1:eq(#id)&&a"//列表
     },
     搜索: '*',
 }
