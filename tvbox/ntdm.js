5b6e4cd9dd5ee1de7b6f3b22d5440215caa057cf
yy <111@qq.comqq.com>
2025-10-12 14:13:47 +0800

    1.8

--- tvbox/ntdm.js
+++ tvbox/ntdm.js
@@ -5,7 +5,7 @@ var rule = {
     homeUrl: '/type/riben.html',
     url:'/type/fyclass-fypage.html',
     filterable: 0, //是否启用分类筛选,
-    filter_url: '--{{fl.by}}-{{fl.class}}--{{fl.letter}}---fypage---{{fl.year}}',
+    //filter_url: '--{{fl.by}}-{{fl.class}}--{{fl.letter}}---fypage---{{fl.year}}',
     searchUrl: '/search/**----------fypage---.html',
     searchable: 2, //是否启用全局搜索,
     quickSearch: 0, //是否启用快速搜索,
