bbb9c1cb472d1ea30c9587e6b81ce46aa50f41ab
yy <111@qq.comqq.com>
2025-10-12 22:42:18 +0800

    1.6

--- tvbox/jcy.js
+++ tvbox/jcy.js
@@ -12,7 +12,7 @@ var rule = {
     headers: { //网站的请求头,完整支持所有的,常带ua和cookies
         'User-Agent': 'MOBILE_UA',
     },
-    class_parse: '.navbar&&ul&&li;a&&title;a&&href;.*/(\\w+).html',
+    class_parse: '.navbar&&ul&&li:eq(0);a&&title;a&&href;.*/(\\w+).html',
     cate_exclude: '追剧周表|热榜|APP',
     play_parse: true,
     detailUrl: '',
