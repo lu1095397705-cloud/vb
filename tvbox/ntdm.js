d3cee33f1bd04fb3221c67f5840c98d194ddb0a3
yy <111@qq.comqq.com>
2025-10-12 22:25:58 +0800

    4.2

--- tvbox/ntdm.js
+++ tvbox/ntdm.js
@@ -2,7 +2,7 @@
 var rule = {
     title: 'NT动漫',
     host: 'http://www.ntdm8.com',
-    //homeUrl: '/type/riben.html',
+    homeUrl: '/type/riben.html',
     url:'/type/fyclass-fypage.html',
     filterable: 0, //是否启用分类筛选,
     //filter_url: '--{{fl.by}}-{{fl.class}}--{{fl.letter}}---fypage---{{fl.year}}',
