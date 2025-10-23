29331c0503f63cd36077f5bc5cb75762c46ee2e7
yy <111@qq.comqq.com>
2025-10-23 22:37:08 +0800

    2.1

--- tvbox/kr.js
+++ tvbox/kr.js
@@ -28,7 +28,7 @@ var rule = {
         "img": ".lazy mask-1&&data-src",
         "content": "",//简介
         "tabs": ".swiper-wrapper&&a",//线路名
-        "lists": "#panel1:eq(#id)&&a"//列表
+        "lists": ".anthology-list-play size:eq(#id)&&ul"//列表
     },
     搜索: '*',
 }
