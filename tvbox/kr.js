6e48253e625739eb9f4cc4bf377f53ec6ccc433c
yy <111@qq.comqq.com>
2025-10-24 19:50:14 +0800

    3.2

--- tvbox/kr.js
+++ tvbox/kr.js
@@ -7,8 +7,8 @@ var rule = {
     filterable: 0, //是否启用分类筛选,
     //filter_url: '--{{fl.by}}-{{fl.class}}--{{fl.letter}}---fypage---{{fl.year}}',
     searchUrl: '/vodsearch/-------------.html?wd=**',
-    searchable: 2, //是否启用全局搜索,
-    quickSearch: 0, //是否启用快速搜索,
+    searchable: 1, //是否启用全局搜索,
+    quickSearch: 1, //是否启用快速搜索,
     headers: { //网站的请求头,完整支持所有的,常带ua和cookies
         'User-Agent': 'MOBILE_UA',
     },
