376ede7080bf80fae46f144aa888ce770bfcebde
yy <111@qq.comqq.com>
2025-10-24 14:20:41 +0800

    4.2

--- tvbox/kr.js
+++ tvbox/kr.js
@@ -27,7 +27,7 @@ var rule = {
         "title": ".h3&&Text;.slide-info-remarks&&Text",//名称；状态
         "img": ".lazy mask-1&&data-src",
         "content": "",//简介
-        "tabs": ".swiper-wrapper:eq(0)&&a",//线路名
+        "tabs": "一线|二线|三线|四线",//线路名
         "lists": ".anthology-list-play:eq(#id)&&li"//列表
     },
     搜索: '*',
