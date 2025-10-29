a799ca156952c7a2cbccf68bdb7703a4986c8b54
yy <111@qq.comqq.com>
2025-10-29 18:54:42 +0800

    4

---
+++ tvbox/js/nxdm.js
@@ -0,0 +1,36 @@
+//https://www.mxdm.xyz/
+var rule ={
+    title: 'mx动漫',
+    host: 'https://www.mxdm.xyz',
+    homeUrl: '/show/fyclass--------fypage---.html',
+    url: '/show/fyclass--------fypage---.html',
+    filterable: 0, //是否启用分类筛选,
+    //filter_url: '--{{fl.by}}-{{fl.class}}--{{fl.letter}}---fypage---{{fl.year}}', 
+    searchUrl: '/search/-------------.html?wd=**',
+    searchable: 2, //是否启用全局搜索,
+    quickSearch: 1, //是否启用快速搜索,
+    headers: { //网站的请求头,完整支持所有的,常带ua和cookies
+        'User-Agent': 'MOBILE_UA',
+    },
+    timeout: 5000,
+    class_parse: '.nav-menu-items li;a&&title;a&&href;.*/(\\w+).html',
+    cate_exclude: '推荐',
+    play_parse: true,
+    detailUrl: '',
+    lazy: "",
+    limit: 6,
+    推荐: '*',
+    double: true, // 推荐内容是否双层定位
+    一级: '.blockcontent1&&.blockdif2;img&&alt;img&&src;.newname&&Text;a&&href',//列表；名称；图片，剧集数；链接,
+    二级访问前: '',
+    二级: {
+        "title": "h4&&Text;.detail_imform_value:eq(6)&&Text",//名称；状态
+        "img": ".poster&&src",
+        "desc": "",
+        "content": ".detail_imform_desc_pre&&Text",//简介
+        "tabs": "js:TABS=['小卢快线','小卢二线','🦌','少','点','了']",//线路
+        "lists": ".movurl:eq(#id)&&li"//列表
+    },
+    搜索: '*',
+
+}
