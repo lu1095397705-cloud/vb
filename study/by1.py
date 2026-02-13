bb2e44278f00b88a4962e819243a50f404bd945c
yy <111@qq.comqq.com>
2026-02-13 19:31:57 +0800

    1

--- study/by1.py
+++ study/by1.py
@@ -1,26 +1,14 @@
 
 import requests
 from bs4 import BeautifulSoup
-url='https://666.666291.xyz/index.php/vod/detail/id/178.html'
+url='https://woog.nxog.eu.org/index.php/vod/detail/id/3225.html'
 res=requests.get(url,headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36'})
 # print(res.content.decode('utf-8'))
-resp=BeautifulSoup(res.content,'html.parser')
-href=[]
-for i in resp.find_all('div',class_="module-row-shortcuts"):
-    url = i.find('a').get('href')
-    href.append(url)
-play_url='$$$'.join(href)
-xll=[]
-div_list = resp.find_all('div', class_='module-tab-content')
-if len(div_list) >= 2:
-    second_div = div_list[1]
-    spans = second_div.find_all('span')
-    names = []
-    for span in spans:
-        val = span.get('data-dropdown-value')
-        if val:
-            names.append(val)
-    play_name = '$$$'.join(names)
+resp = BeautifulSoup(res.content, 'html.parser')
+
+
+
+
 
 
 
--- tvbox/py/ouge.py
+++ tvbox/py/ouge.py
@@ -109,24 +109,21 @@ class Spider(Spider):
         res = self.fetch(url, headers=self.get_headers(url), cookies=self.cookies, timeout=10)
         if res.cookies.get_dict():
             self.cookies.update(res.cookies.get_dict())
-        resp = BeautifulSoup(res.content, 'html.parser')
         href = []
-        for i in resp.find_all('div', class_="module-row-shortcuts"):
-            url = i.find('a').get('href')
+        resp = BeautifulSoup(res.text, 'html.parser')
+        for i in resp.find_all('div', class_="module-row-one"):
+            url = i.find('p').text
             href.append(url)
         play_url = '$$$'.join(href)
-        xll = []
-        div_list = resp.find_all('div', class_='module-tab-content')
-        if len(div_list) >= 2:
-            second_div = div_list[1]
-            spans = second_div.find_all('span')
-            names = []
-            for span in spans:
-                val = span.get('data-dropdown-value')
-                if val:
-                    names.append(val)
-            play_name = '$$$'.join(names)
 
+        div_list = resp.find('div', class_='module-tab-content')
+        spans = div_list.find_all('span')
+        names = []
+        for span in spans:
+            val = span.get('data-dropdown-value')
+            if val:
+                names.append(val)
+        play_name = '$$$'.join(names)
         vod = {
         "vod_id": ids[0],
         "vod_name":'',
